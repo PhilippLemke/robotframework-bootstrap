9ade07dbfacddb7e9d4ca206849114fe67c2e694
Philipp Lemke <pl@automation-monitoring.com>
2024-07-04 08:33:30 +0200

    handle case: no extensions installed

--- salt-data/srv/env/base/_modules/vscode.py
+++ salt-data/srv/env/base/_modules/vscode.py
@@ -42,12 +42,25 @@ def uninstall_extension(extension, version=None, vscode_cmd=def_vscode_cmd):
 def list_extensions(vscode_cmd=def_vscode_cmd):
     """
     List all installed VSCode extensions
+    Returns them as a list of tuples (ext, version)
+    Example:
+    extension list = [
+        ('d-biehl.robotcode', '0.83.3'),
+        ('ms-python.debugpy', '2024.6.0'),
+        ('ms-python.python', '2024.6.0'),
+        ('ms-python.vscode-pylance', '2024.6.1')]
     """
     cmd = [vscode_cmd, "--list-extensions", '--show-versions']
     try:
         ret = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
         ret = ret.decode().strip()
-        return [(ext.split('@')[0], ext.split('@')[1]) for ext in ret.split('\n')]
+        # vscode command sucessfull but no extensions installed
+        if ret == "":
+            extension_list = []
+        # vscode command sucessfull, extensions listed and transformed into list of tuples
+        else:  
+            extension_list = [(ext.split('@')[0], ext.split('@')[1]) for ext in ret.split('\n')]
+        return extension_list
         
     except subprocess.CalledProcessError as e:
         log.error(f"Failed to list extensions: {e.output}")
